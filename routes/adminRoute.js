b578c8a85bddd34f1d1b501b0d7e7aa606f1fb25
alozievictor <aloziev50@gmail.com>
2023-01-06 03:11:42 +0100

    admin section done

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router()
 const adminLoginSchema = require('../models/admin')
+const QuestionSchema = require('../models/question')
 const bcrypt = require('bcrypt');
 const validator = require('validator');
 
 
 router.get('/admin', (req, res) => {
-    res.render('admin');
+    res.render('adminReg',{message:''});
 })
 
 router.get('/Login', (req, res) => {
-    res.render('adminlogin')
+    res.render('adminlogin',{message:''})
+})
+
+router.get('/adminPage', async (req, res) => {
+    try {
+        const Questions = await QuestionSchema.find() 
+        if(Questions) {
+            return res.render('admin',{Questions:Questions, message:''})
+        }
+
+        return res.status(400).redirect('/Login',{message:''})
+
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 
 
@@ -48,16 +64,15 @@ router.post('/RegAdmin', async (req, res) => {
         if(error) {
             return res.render('adminReg',{message:error})
         }
-
         const Hash = bcrypt.hashSync(userData.password, 5)
         userData.password = Hash
         await adminLoginSchema.create(userData);
-        consoler.log('email and password Posted')
+        console.log('email and password Posted')
         return res.render('adminlogin', {message:'Success'})
 
     } catch (error) {
         console.log(error)
-        res.render('/adminReg')
+        res.render('adminReg')
     }
 })
 
@@ -71,25 +86,53 @@ router.post('/LoginAdmin', async (req, res) => {
 
         const error = validateData(userData)
         if(error) {
-            return res.render('/', {message:error})
+            console.log('validation checked')
+            return res.render('adminlogin',{message:error})
         }
 
         const checkAdmin = await adminLoginSchema.findOne({email:userData.email})
+        console.log(checkAdmin)
         if(checkAdmin.email !== userData.email) {
             return res.render('adminlogin',{message:process.env.EmailErr})
         }
+        console.log('email checked')
 
         const decryptPassword = await bcrypt.compareSync(userData.password, checkAdmin.password)
         if(decryptPassword !== true) {
             return res.render('adminlogin',{message:process.env.PasswordErr})
         } 
+        console.log('password checked')
 
-        return res.render('admin',{message:process.env.success})
+        console.log('Verification completed')
+        return res.status(200).redirect('/adminPage')
 
     } catch (error) {
         console.log(error) 
-        res.render('adminlogin')
+       return res.render('adminlogin',{message:process.env.ErrMsg})
     }
+})
+
+
+router.get('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        if(id.length !== 24) {
+            return res.status(404).redirect('/adminPage')
+        }
+
+        const checkQuestion = await QuestionSchema.findOne({_id:id})
+        if(!checkQuestion) {
+            return res.status(404).redirect('/adminPage')
+        }
+        
+        const DeleteQuestion = await QuestionSchema.findOneAndDelete({_id:id}) 
+        return res.status(200).redirect('/adminPage')
+
+    } catch (error) {
+        console.log(error)
+    }
+    
 })
 
 module.exports = router;
